51452b98bd84953e61d66dffaf4d7dbc16f8d1c3
michalglomsky <michal.glomsky@gmail.com>
2026-03-22 11:05:49 +0100

    Remove old demo files from main

--- src/react_agent/state.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, Annotated, List
-from langchain_core.messages import BaseMessage
-
-class AgentState(TypedDict):
-    messages: Annotated[List[BaseMessage], lambda x, y: x + y]
-    transcriptions: List[str]
-    dataframe: list[dict]
-    processed_files: List[str]
